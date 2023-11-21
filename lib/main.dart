881b7f07343131f44a835dd62d40becb0ce21ee8
Mahmoud Nairat <mahmoud.a.nairat@gmail.com>
2023-11-21 13:50:21 +0200

    Add phone and email

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,9 @@ class BusinessCardApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Color(0xff28475e),
+        backgroundColor: const Color(0xff2B475e),
         body: Column(
           children: [
             // ClipRRect(
@@ -23,7 +23,7 @@ class BusinessCardApp extends StatelessWidget {
             //     //Shourtcut for this => image.asset() / image.network
             //   ),
             // ),
-            CircleAvatar(
+            const CircleAvatar(
               radius: 122,
               backgroundColor: Colors.white,
               child: CircleAvatar(
@@ -31,7 +31,7 @@ class BusinessCardApp extends StatelessWidget {
                 backgroundImage: AssetImage('assets/mahmoud.png'),
               ),
             ),
-            Text(
+            const Text(
               'Mahmoud Nairat',
               style: TextStyle(
                 color: Colors.white,
@@ -39,6 +39,86 @@ class BusinessCardApp extends StatelessWidget {
                 fontFamily: 'Pacifico',
               ),
             ),
+            const Text(
+              'FLUTTER DEVELOPER',
+              style: TextStyle(
+                color: Color(0xFF6c8890),
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                  height: 65,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: const Row(
+                    children: [
+                      //Here at the first I can remove Spacer() and wrap icon widget with padding widget and make padding left ex 16
+                      Spacer(
+                        flex: 1,
+                      ),
+                      Icon(
+                        Icons.phone,
+                        size: 32,
+                        color: Color(0xff2B475e),
+                      ),
+                      Spacer(
+                        flex: 1,
+                      ), //This widget make space between two widgets inside row or column.
+                      Text(
+                        '(+972) 597323291',
+                        style: TextStyle(
+                          color: Color(0xff2B475e),
+                          fontSize: 20,
+                        ),
+                      ),
+                      Spacer(
+                        flex: 3,
+                      ),
+                    ],
+                  ),
+                ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                  height: 65,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: const Row(
+                    children: [
+                      //Here at the first I can remove Spacer() and wrap icon widget with padding widget and make padding left ex 16
+                      Spacer(
+                        flex: 1,
+                      ),
+                      Icon(
+                        Icons.mail,
+                        size: 32,
+                        color: Color(0xff2B475e),
+                      ),
+                      Spacer(
+                        flex: 1,
+                      ), //This widget make space between two widgets inside row or column.
+                      Text(
+                        'mahmoud.a.nairat@gmail.com',
+                        style: TextStyle(
+                          color: Color(0xff2B475e),
+                          fontSize: 20,
+                        ),
+                      ),
+                      Spacer(
+                        flex: 3,
+                      ),
+                    ],
+                  ),
+                ),
+            )
           ],
         ),
       ),
